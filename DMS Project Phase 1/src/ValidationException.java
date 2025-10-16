ebbfabbfe3916ef147158e5da267f93c4c731790
theFonzii <101096626+theFonzii@users.noreply.github.com>
2025-10-16 14:47:41 -0400

    Add files via upload

---
+++ DMS Project Phase 1/src/ValidationException.java
@@ -0,0 +1,11 @@
+/**
+ * David Alfonsi
+ * CEN 3024C - Software Development 1
+ * October 7th, 2025
+ * ValidationException.java
+ * This checked exception is used whenever user input fails validation.
+ */
+
+public class ValidationException extends Exception {
+    public ValidationException(String message) { super(message); }
+}
